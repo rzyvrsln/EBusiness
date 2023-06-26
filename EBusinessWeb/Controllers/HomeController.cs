da6122b433d3632892c3de2bc0607994f30d6a0c
rzyvrsln <rzayevruslan893@gmail.com>
2023-06-26 22:27:32 +0400

    asp.netcore

--- EBusinessWeb/Controllers/HomeController.cs
+++ EBusinessWeb/Controllers/HomeController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EBusinessService.Services.Abstraction;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EBusinessWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEmployeeService employeeService;
+
+        public HomeController(IEmployeeService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+
         [HttpGet]
-        public async Task<IActionResult> Index() => View();
+        public async Task<IActionResult> Index()
+        {
+            var employees = await employeeService.GetAllEmployeeAsync();
+            return View(employees);
+        }
     }
 }
